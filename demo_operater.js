d19bb0c2a7fe778ed23de2505616aa073a9d4f76
levanhuan99 <61698248+levanhuan99@users.noreply.github.com>
2020-03-07 11:39:57 +0700

    biến và kiểu dữ liệu

---
+++ demo_operater.js
@@ -0,0 +1,3 @@
+let inputWidth =parseInt(prompt('enter the width'));
+let inputHeight = parseInt(prompt('enter the height'));
+alert('dien tich hinh chu nhat la = ' +inputHeight*inputWidth);
---
+++ type_of_data.js
@@ -0,0 +1,13 @@
+function showMark() {
+
+    let physical=parseFloat(prompt('nhap diem vat ly'));
+    let chemistry=parseFloat(prompt('nhap diem hoa hoc'));
+    let biology=parseFloat(prompt('nhap diem sinh hoc'));
+    document.write('diem trung binh = ' +((physical+chemistry+biology)/3));
+
+
+}
+/*function changeTemprature() {
+    let cDegree=
+
+}
